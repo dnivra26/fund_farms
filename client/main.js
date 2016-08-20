486badf0a3b6d99433b2e50eee60935cd4cb3eb8
Dinesh Balasubramanian <dineshudt17@gmail.com>
2016-08-20 12:13:06 +0530

    Add google maps... need to hook in right place

--- client/main.js
+++ client/main.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import { Meteor } from 'meteor/meteor';
-import { render } from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router'
-import App from '../imports/ui/App.jsx';
-import FarmPlanner from '../imports/ui/FarmPlanner.jsx';
-import FarmList from '../imports/ui/FarmList.jsx';
-import InvestorPlan from '../imports/ui/InvestorPlan.jsx';
-import FarmerFarmList from '../imports/ui/FarmerFarmList.jsx';
-import FarmerPlan from '../imports/ui/FarmerPlan.jsx';
-import Login from '../imports/ui/Login.jsx';
+import React from "react";
+import {Meteor} from "meteor/meteor";
+import {render} from "react-dom";
+import {Router, Route, browserHistory} from "react-router";
+import FarmPlanner from "../imports/ui/FarmPlanner.jsx";
+import FarmList from "../imports/ui/FarmList.jsx";
+import InvestorPlan from "../imports/ui/InvestorPlan.jsx";
+import FarmerFarmList from "../imports/ui/FarmerFarmList.jsx";
+import FarmerPlan from "../imports/ui/FarmerPlan.jsx";
+import Login from "../imports/ui/Login.jsx";
 
 Meteor.startup(() => {
   render(
@@ -16,7 +15,6 @@ Meteor.startup(() => {
       <Route path="/" component={Login}/>
       <Route path="/user/:userId/farmList" component={FarmList}/>
       <Route path="/user/:userId/crops/:cropId/investorPlan" component={InvestorPlan}/>
-
       <Route path="/user/:userId/farmerFarmList" component={FarmerFarmList}/>
       <Route path="/user/:userId/crops/:cropId/farmerPlan" component={FarmerPlan}/>
       <Route path="/user/:userId/farmPlanner" component={FarmPlanner}/>
--- imports/ui/Map.jsx
+++ imports/ui/Map.jsx
@@ -4,22 +4,26 @@ import GoogleMap from 'google-map-react';
 
 export default class Map extends Component {
 
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-    return (
-      <GoogleMap
-        defaultCenter={this.props.center}
-        defaultZoom={this.props.zoom}>
-      </GoogleMap>
-    );
-  }
+    render() {
+        return (
+            <div id="mapHeight"><GoogleMap
+                bootstrapURLKeys={{
+                    key: 'FILL_IN_API_KEY',
+                    language: 'us'}}
+                defaultCenter={this.props.center}
+                defaultZoom={this.props.zoom}>
+            </GoogleMap>
+            </div>
+        );
+    }
 }
 
 Map.defaultProps = {
-  center: {lat: 59.938043, lng: 30.337157},
-  zoom: 9,
-  greatPlaceCoords: {lat: 59.724465, lng: 30.080121}
+    center: {lat: 59.938043, lng: 30.337157},
+    zoom: 9,
+    greatPlaceCoords: {lat: 59.724465, lng: 30.080121}
 };
